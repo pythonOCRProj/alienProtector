7d8522a0b11200815a8803098702136394d3d722
sj <zkdlwjsxm@nate.com>
2024-03-11 21:53:01 +0900

    업로드

--- src/main/java/notice_p/NoticeDelete.java
+++ src/main/java/notice_p/NoticeDelete.java
@@ -1,23 +1,25 @@
 package notice_p;
-
 import dao_p.NoticeDAO;
-
 import dto_p.NoticeDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service_p.NoticeService;
 
-public class NoticeDetail implements NoticeService  {
-	
+
+public class NoticeDelete  implements NoticeService{
+
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		// TODO Auto-generated method stub
 		int no = Integer.parseInt(request.getParameter("no"));
-		new NoticeDAO().noticeCnt(no);
+		
 		NoticeDTO dto = new NoticeDAO().detail(no);
-		request.setAttribute("noticeDetail", dto);
-		System.out.println("노티스디테일자바 진입");
+		
+		new NoticeDAO().delete(no);
+		System.out.println("노티스삭제JAVA진입");
+		request.setAttribute("incUrl", "notice/NoticeDelete.jsp");
 		
 		
 	}
+	
 }
