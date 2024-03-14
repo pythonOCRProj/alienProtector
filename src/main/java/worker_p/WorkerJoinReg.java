329d5fe0fb30cbc0d552d55d2253e570dafe31a4
pms <pmsqx7@naver.com>
2024-03-14 18:12:10 +0900

    0314 18:12 03

--- src/main/java/worker_p/WorkerJoinReg.java
+++ src/main/java/worker_p/WorkerJoinReg.java
@@ -1,8 +1,11 @@
 package worker_p;
 
+import java.io.IOException;
+
 import dao_p.WorkerDAO;
 import dto_p.WorkerDTO;
 import etc_p.RedirectionPage;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service_p.WorkerService;
@@ -14,23 +17,48 @@ public class WorkerJoinReg implements WorkerService {
 		
 		try {
 		
-			WorkerDTO dto = new WorkerDTO();
+			WorkerDTO worker = new WorkerDTO();
 			
-			dto.setId(request.getParameter("id"));
+			worker.setId(request.getParameter("id"));
+			
+			worker = new WorkerDAO().getWorkerInfo(worker);
+			
+			String pwd = request.getParameter("pwd");
+			String pwdchk = request.getParameter("pwdchk");
+			String phone_num = request.getParameter("phone_num");
+			
+			String addr1 = request.getParameter("addr1");
+			String addr2 = request.getParameter("addr2");
+			String addr3 = request.getParameter("addr3");
+			String addr4 = request.getParameter("addr4");
+			
 			dto.setPwd(request.getParameter("pwd"));
-			
 			dto.setEmail(request.getParameter("email"));
 			dto.setPhoneNum(request.getParameter("phone_num"));
 			dto.setName(request.getParameter("name"));
 			dto.setAddr(request.getParameter("addr"));
 			dto.setProfileImg(request.getParameter("profile_img"));
 			
-			int cnt = new WorkerDAO().join(dto);
-			//System.out.println("회원가입입니다:"+cnt);
+			try {
 			
+				//각종 유효성 검사들
+				if(!"".equals(pwd) && pwd != null && pwd.equals(pwdchk)) {
+					worker.setPwd(pwd);
+				} else(!pwd.equals(pwdchk)) {
+					
+				} 
+				
 			
-			new RedirectionPage(request, response).movePage("근무자 등록 완료", "/alienProtector/worker/WorkerList");
-		
+				int cnt = new WorkerDAO().join(dto);
+				
+				new RedirectionPage(request, response).movePage("근무자 등록 완료", "/alienProtector/worker/WorkerList");
+			
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (ServletException e) {
+				e.printStackTrace();
+			}
+				
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
